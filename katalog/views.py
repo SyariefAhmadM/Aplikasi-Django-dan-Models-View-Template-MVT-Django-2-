863241fb3c60ff76953a0c2158f71ade4b549290
Syarief Ahmad Al-Muhajir <syariefam305@gmail.com>
2022-09-14 02:32:57 +0700

    Pengumpulan Tugas-2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_catalog': data_katalog_item,
+        'nama': 'mas Syarief',
+        'npm': '2106653445'
+    }
+    return render(request, "katalog.html", context)
