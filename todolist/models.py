ba7c607d8e15b2ef00e28d934b5ec61da753b8a6
Syarief Ahmad Al-Muhajir <syariefam305@gmail.com>
2022-09-29 11:41:10 +0700

    Tugas-4

--- todolist/models.py
+++ todolist/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE
 from django.contrib.auth import get_user_model
 User = get_user_model()
 # Create your models here.
