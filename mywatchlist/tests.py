d2cb666857c02a9f17176534b92447e4bf96c6ca
Syarief Ahmad Al-Muhajir <syariefam305@gmail.com>
2022-10-06 11:17:57 +0700

    Tugas-5

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -4,16 +4,12 @@ from django.urls import resolve
 
 # Create your tests here.
 class AppTest(TestCase):
-    def test_app_url_exists(self):
+    def test_app_url_exists1(self):
         response = Client().get('http://localhost:8000/mywatchlist/html/')
         self.assertEqual(response.status_code,200)
-
-class AppTest(TestCase):
-    def test_app_url_exists(self):
+    def test_app_url_exists2(self):
         response = Client().get('http://localhost:8000/mywatchlist/xml/')
         self.assertEqual(response.status_code,200)
-
-class AppTest(TestCase):
-    def test_app_url_exists(self):
+    def test_app_url_exists3(self):
         response = Client().get('http://localhost:8000/mywatchlist/json/')
         self.assertEqual(response.status_code,200)
---
+++ todolist/README(2).md
@@ -0,0 +1,4 @@
+1. Apa perbedaan dari Inline, Internal, dan External CSS? Apa saja kelebihan dan kekurangan dari masing-masing style?
+2. Jelaskan tag HTML5 yang kamu ketahui.
+3. Jelaskan tipe-tipe CSS selector yang kamu ketahui.
+4. Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas.
